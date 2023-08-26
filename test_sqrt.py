1778d31dc354081990c565f89222ba9a92d430fe
Neeraj Sharma <nksharma063@gmail.com>
2023-08-27 01:27:15 +0530

    WIP:modified the stest script

--- test_sqrt.py
+++ test_sqrt.py
@@ -9,6 +9,8 @@ load_dotenv()
 
 token = os.environ['GIT_TOKEN']
 
+auth_token = os.environ['GIT_AUTH_TOKEN']
+
 commits = subprocess.check_output(['curl', '-L'  , '-H' ,'Accept: application/vnd.github+json',   '-H', 'token',   '-H', 'X-GitHub-Api-Version: 2022-11-28',   'https://api.github.com/repos/nksharma063/CICD_Project/commits?sha=dev'])
 
 commits = json.loads(commits.decode('utf-8'))
@@ -20,13 +22,53 @@ comments = subprocess.check_output(['curl', '-L'  , '-H' ,'Accept: application/v
 
 comments = json.loads(comments.decode('utf-8'))
 comments = comments[0]['body']
-
 print(comments)
+
+
+# Set the repository and commit information
+owner = 'nksharma063'
+repo = 'CICD_Project'
+sha = COMMITID
+
+# Set the status information
+state = 'success'
+context = 'continuous-integration/jenkins'
+description = 'The build succeeded!'
+
+# Create the commit status
+cmd = ['curl', '-X', 'POST', '-H', f'Authorization: Bearer {auth_token}', '-H', 'Content-Type: application/json', '-d', f'{{"state":"{state}", "context":"{context}", "description":"{description}"}}', f'https://api.github.com/repos/{owner}/{repo}/statuses/{sha}']
+
+
+
+
+# print(data_status)
+
+commits = subprocess.check_output(['curl', '-L'  , '-H' ,'Accept: application/vnd.github+json',   '-H', 'token',   '-H', 'X-GitHub-Api-Version: 2022-11-28',   f'https://api.github.com/repos/nksharma063/CICD_Project/commits/{COMMITID}/status'])
+
+data = json.loads(commits.decode('utf-8'))
+print(data['state'])
+
+# def test_hello():
+#     a = hello()
+#     assert a == "hi breakout 4"
+
+# def test_registration():
+#     a = registration()
+#     assert a == "This is registration page"
+
+# if __name__ == "__main__":
+#     exit_code = sys.exit(pytest.main(["-x", "test_sqrt.py"]))
+#     # result = (exit_code == 0)
+#     # print(result)
+
 
 
 
 """
-Rough work
+
+# Update the commit status
+# data_status = subprocess.check_output(['curl', '-L', '-X', 'POST', '-H', 'Accept: application/vnd.github+json', '-H', f'Authorization: Bearer {auth_token}', f'https://api.github.com/repos/nksharma063/CICD_Project/statuses/{COMMITID}', '-d' , '-d', '{"state":"success","context":"continuous-integration/jenkins"}'])
+
 
 from datetime import datetime, timedelta
 
@@ -42,9 +84,6 @@ headers = {
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
-"""
-
-"""
 Another rough work
 
 # commit_data = commits.json()
@@ -55,21 +94,7 @@ Another rough work
 
 # print(commit_ids)
 
+#print(comment["parents"]["sha"][0])
 """
 
-
-
-#print(comment["parents"]["sha"][0])
-# def test_hello():
-#     a = hello()
-#     assert a == "hi breakout 4"
-
-# def test_registration():
-#     a = registration()
-#     assert a == "This is registration page"
-
-# if __name__ == "__main__":
-#     exit_code = sys.exit(pytest.main(["-x", "test_sqrt.py"]))
-#     # result = (exit_code == 0)
-#     # print(result)
 
