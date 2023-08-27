c7261759947b731bcd7626dc6d7809eeb87c954d
Neeraj Sharma <nksharma063@gmail.com>
2023-08-28 01:50:22 +0530

    Add: new fucntion added in code

--- sqrt.py
+++ sqrt.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return f"hi breakout 4"
 
+@app.route('/registration')
+def registration():
+    return f"Welcome to registration room"
+
+
 if __name__ == '__main__':
     app.run(port=3000, debug=True)    
