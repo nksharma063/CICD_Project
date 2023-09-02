8bd518214307935a13cedaf1f290429840adaaaf
Neeraj <nksharma063@gmail.com>
2023-09-02 21:59:27 +0530

    Add: added description for registrarion and error logger file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys, os
 import logging
 
 app = Flask(__name__)
-logging.basicConfig(filename="record.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadname)s : %(message)s')
+logging.basicConfig(filename="record_app.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadname)s : %(message)s')
 
 @app.route('/hello')
 def hello():
@@ -11,8 +11,13 @@ def hello():
 
 @app.route('/registration')
 def registration():
-    return f"Welcome to registration room"
+    return f"Welcome to registration room and will do add some content later"
 
+@app.route('/error')
+def error():
+    logger.error(f'erro1')
+    logger.info(f'info1')
+    logger.critical(f'detail1')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)    
