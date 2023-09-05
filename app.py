4026be5c3ae98b1265803c843e2035362e2d6bff
Neeraj <nksharma063@gmail.com>
2023-09-05 22:55:59 +0530

    Automatic commit after cherry-picking and pushing changes to the test branch

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 import sys, os
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 app = Flask(__name__)
 =======
 import logging
+=======
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(filename="record_app.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+>>>>>>> 0831f26 (Add: new fucntions, updated .gitignore file and some logging fucntions, also testig the pipeline)
 
 app = Flask(__name__)
 logging.basicConfig(filename="record_app.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
@@ -24,12 +31,19 @@ def registration():
 def aboutus():
     return f"Welcome to about us page"
 
+@app.route('/aboutus')
+def aboutus():
+    return f"Welcome to about us page"
+
 @app.route('/error')
 def error():
     app.logger.error(f'erro1')
     app.logger.info(f'info1')
     app.logger.critical(f'detail1')
     return f'Loggin error page'
+<<<<<<< HEAD
+>>>>>>> 0831f26 (Add: new fucntions, updated .gitignore file and some logging fucntions, also testig the pipeline)
+=======
 >>>>>>> 0831f26 (Add: new fucntions, updated .gitignore file and some logging fucntions, also testig the pipeline)
 
 if __name__ == '__main__':
