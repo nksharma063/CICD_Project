0831f2611da64087f638b595f490b3d629e10173
Neeraj <nksharma063@gmail.com>
2023-09-05 22:55:59 +0530

    Add: new fucntions, updated .gitignore file and some logging fucntions, also testig the pipeline

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 import sys, os
-from flask import logging
+import logging
 
 app = Flask(__name__)
-logging.basicConfig(filename="record_app.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadname)s : %(message)s')
+logging.basicConfig(filename="record_app.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route('/hello')
 def hello():
@@ -13,11 +13,16 @@ def hello():
 def registration():
     return f"Welcome to registration room and will do add some content later"
 
+@app.route('/aboutus')
+def aboutus():
+    return f"Welcome to about us page"
+
 @app.route('/error')
 def error():
-    logger.error(f'erro1')
-    logger.info(f'info1')
-    logger.critical(f'detail1')
+    app.logger.error(f'erro1')
+    app.logger.info(f'info1')
+    app.logger.critical(f'detail1')
+    return f'Loggin error page'
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)    
+    app.run(port=3000, debug=True)
